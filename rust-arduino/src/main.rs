1d249a018e5cf3ea57aff4a605b6e80c53ea9957
dark0ghost <dark0ghost@debian.org>
2020-09-07 13:09:40 +0300

    rust-arduino

--- rust-arduino/src/main.rs
+++ rust-arduino/src/main.rs
@@ -1,3 +1,16 @@
+#[warn(unused_imports)]
+use arduino::{
+    PORTB, DDRB
+}; // Register
+use arduino::PORTB7;
+use arduino::prelude::without_interrupts;
+use std::ptr::write_volatile; // Pin
+
 fn main() {
+    without_interrupts(|| { //Disable interrupts.
+        unsafe {
+            write_volatile(DDRB, 0xFF)
+        }
+    });
     println!("Hello, world!");
 }
