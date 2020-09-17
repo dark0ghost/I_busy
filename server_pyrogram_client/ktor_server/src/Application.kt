8fd0b4f8999d927966cab6a1cb64043cb4f8f246
dark0ghost <dark0ghost@debian.org>
2020-09-17 22:54:50 +0300

    change jdk15 -> jdk8, no support

--- server_pyrogram_client/ktor_server/src/Application.kt
+++ server_pyrogram_client/ktor_server/src/Application.kt
@@ -3,8 +3,20 @@ package com.openproject
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit {
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
