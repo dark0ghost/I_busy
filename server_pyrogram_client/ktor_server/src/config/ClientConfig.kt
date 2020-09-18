0d7431e10b5741240b2976d46113fea5ef5597eb
dark0ghost <dark0ghost@debian.org>
2020-09-18 23:12:14 +0300

    make load config

--- server_pyrogram_client/ktor_server/src/config/ClientConfig.kt
+++ server_pyrogram_client/ktor_server/src/config/ClientConfig.kt
@@ -1,5 +1,13 @@
 package com.openproject.config
 
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import java.io.FileInputStream
+
+
 data class ClientConfig(
   public val app_api_id: String,
   public val app_api_hash: String,
@@ -8,4 +16,24 @@ data class ClientConfig(
   public val product_configuration: String,
   public val trigger_username: String,
   public val message_on_trigger: String
-)
+) {
+    companion object {
+        suspend fun loadDataFromJsonFile(path: String = "../src/config/client.json") {
+
+            val contents = withContext(Dispatchers.IO) {
+                FileInputStream(path).use {
+                    it.readBytes()
+                }
+
+            }
+            val json = Json.encodeToJsonElement(contents)
+            println(json)
+
+
+        }
+
+
+    }
+}
+
+
