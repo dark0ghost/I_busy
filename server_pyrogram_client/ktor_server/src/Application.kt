52400b331839e4bf67df79baca2fce3433a3afc6
dark0ghost <dark0ghostworkmail@seznam.cz>
2020-11-02 22:35:27 +0300

    add arduino code

--- server_pyrogram_client/ktor_server/src/Application.kt
+++ server_pyrogram_client/ktor_server/src/Application.kt
@@ -1,23 +1,24 @@
-package com.openproject
+package
 
-import com.openproject.config.Config
 import com.openproject.lib.loadConfig
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.kotlin.konan.file.File
 
 
 fun main(args: Array<String>): Unit {
-    val config: Config = loadConfig() as Config
+    val config = loadConfig()
     embeddedServer(Netty, port = config.server.port) {
         routing {
             get(path = "/api/command/"){
 
+
             }
             get(path = "/api/"){
-
+               call.respond(File("../src/server_settings/html/doc_api.html"))
             }
         }
     }.start(wait = true)
